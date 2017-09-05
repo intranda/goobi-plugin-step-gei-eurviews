de7dedd1aa0c3e0162edff614590d41e42e7ad3b
Florian Alpers <florian.alpers@intranda.com>
2017-09-05 15:40:03 +0200

    prepared EurViewImport plugin

---
+++ GeiEurViewsPlugins/src/de/intranda/goobi/model/EurViewsRecord.java
@@ -0,0 +1,12 @@
+package de.intranda.goobi.model;
+
+import org.goobi.production.importer.Record;
+
+public class EurViewsRecord extends Record {
+
+    public String getProcess() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
